82964dc407e51dcfe74c6d2281bc38e449fa2dc4
Recremated <111436438+Recremated@users.noreply.github.com>
2025-05-05 13:28:51 +0300

    S1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,44 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (((clock > 16 && clock <= 23) || (clock >= 0 && clock < 8))&& isBarking){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >= 13 && firstAge <=19)||(secondAge >= 13 && secondAge <=19)||(thirdAge >= 13 && thirdAge <=19)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if ((isSummer == true && (temp >= 25 && temp <= 45)) || (isSummer == false && (temp >= 25 && temp <= 35))){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0){
+            return -1;
+        } else {
+            return (width*height);
+        }
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0){
+            return -1;
+        } else {
+            return (radius*radius*Math.PI);
+        }
+
     }
 }
